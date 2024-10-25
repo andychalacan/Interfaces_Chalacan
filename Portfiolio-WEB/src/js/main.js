1414d38ab32e415f7450a391420da83bad7039a8
andychalacan <168015224+andychalacan@users.noreply.github.com>
2024-10-24 19:12:27 -0500

    Portafolio
    Sprin 1 Portafolio-Web

---
+++ Portfiolio-WEB/src/js/main.js
@@ -0,0 +1,62 @@
+// ------------------------- BEGINNER -----------------------
+// Internet
+// TCP-IP
+// ISP = IP - Dispositivo (modem) y (router)
+// WEB (Paginas web + Navegador web)
+// CLIENTE - SERVIDOR (ARQUITECTURA WEB)
+// HTTP (segura y no segura)
+// Domino y Subdominio
+//Hosting y Dominio
+
+//---------------------------- BASIC -------------------------
+//Roles en el Desarrollo Web
+//Backend - Crear APIS
+//Frontend - Crear Interfaces de Uusuario
+//Fullstack - AIO
+
+// Interfaz
+// Interfaz de usuario (UI)
+// Experiencia de usuario (UX)
+// 1.- Header - Body - Footer
+// 2.- Menos es más
+// 3.- Adaptable (XS S M L XXL)
+// 4.- URL AMIGABLE
+
+// ------------------------------ INTERMEDIATE ---------------------
+
+// Fases de Desarrollo de aplicaciones frontend (aplicaciones del lado del cliente)
+// Requisitos (Artefactos) (RR-HU-PB-SB)
+// Wireframe
+// Mockup
+// Prototipo
+
+// ---------------------------------- ADVANCE ---------------------------
+// HTML
+// Etiquetas semánticas body-nav-p-h1-aside-footer-main-ul-ol-figure
+// <a href="#">Nosotros</a> (APERTURA Y CIERRE)
+// Atributos y valores href="#" - loading="lazy"
+// Recursos extras
+
+// CSS (ESTILOS)
+// Reglas (selector - Propiedad - Valor)
+/*
+.button{
+font-size: 18px;
+background-color:red;
+}
+*/
+
+// Diseño Responsive
+// 1.- Media Queries (breakpoints) 
+// 2.- Flexbox
+// 3.- Grid
+
+// Mobile first and deskpot first
+// Deploy
+// Sandbox
+// Testing
+// 1.- Unitarias
+// 2.- Compatibilidad
+// 3.- Carga
+// 4.- Rendimiento
+// 5.- Aceptacion
